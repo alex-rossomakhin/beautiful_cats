2e9509642c397bfbe110178fc705b7dbd22f3565
Alex Ross <alexeirossomahin@yandex.ru>
2023-08-11 13:53:44 +0300

    debag

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_stub_key')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
