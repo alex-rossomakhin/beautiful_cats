a9f7aa7596d9d243bfec634d8a50ce405ba8942e
Alex Ross <alexeirossomahin@yandex.ru>
2023-08-11 20:05:56 +0300

    host3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_stub_key')
 #DEBUG = os.getenv('DEBUG', 'False') == 'True'
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 #ALLOWED_HOSTS = []
 
